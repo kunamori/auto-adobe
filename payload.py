2fd88c828086e540687e6eefe89895753575dd06
Nathawat <158021536+scxtt-nc@users.noreply.github.com>
2025-01-01 16:52:48 +0700

    Update expire date

--- payload.py
+++ payload.py
@@ -66,7 +66,7 @@ adobe_headers = {
 
 adobe_data = {
         "userId": '',
-        "date_expire": "2025-01-01",
+        "date_expire": "2025-02-01",
         "status_number": "0",
         "Submit_get": ''
 }
